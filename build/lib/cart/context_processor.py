08f22612cb92d32bbda75b97153b610985c39499
Hadi Do <dodangeh.hadi@gmail.com>
2020-06-28 12:53:26 +0430

    Any changes in lib

--- build/lib/cart/context_processor.py
+++ build/lib/cart/context_processor.py
@@ -1,11 +1,20 @@
 from .cart import Cart
 
+
 def cart_total_amount(request):
-	if request.user.is_authenticated:
-		cart = Cart(request)
-		total_bill = 0.0
-		for key,value in request.session['cart'].items():
-			total_bill = total_bill + (float(value['price']) * value['quantity'])
-		return {'cart_total_amount' : total_bill} 
-	else:
-		return {'cart_total_amount' : 0} 
+    if request.session.get('cart'):
+        cart = Cart(request)
+        total_bill = 0
+        for key, value in request.session['cart'].items():
+            total_bill = total_bill + (int(value['price']) * value['quantity'])
+        return {'cart_total_amount': total_bill}
+    else:
+        return {'cart_total_amount': 0}
+
+
+# cart item count
+def cart_total_count(request):
+    if request.session.get('cart'):
+        return {'cart_total_count': len(request.session.get('cart').items())}
+    else:
+        return {'cart_total_count': 0}
